bdfd36ba1e7ad20ad5e2bb682fc72fb88b81144e
diogoalexandre <diogomartins.ac@gmail.com>
2019-11-28 08:54:27 -0300

    Initial commit

---
+++ include/pid.hpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+class PID {
+ public:
+        double max, min,
+               kp, ki, kd,
+               setpoint, error, error_prev, error_integral,
+               pv, mv;
+        PID(double max, double min, double kp, double ki, double kd);
+
+        double calculate(double time, double setpoint, double pv);
+};
---
+++ include/smartTurtle.hpp
@@ -0,0 +1,10 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
+
+class SmartTurtle {
+    public:
+
+    private:
+    
+}
